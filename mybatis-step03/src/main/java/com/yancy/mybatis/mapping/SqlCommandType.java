1cea27cd68a80a6dbbe3e42183a74df9c08daf9c
yancy0109 <1156729237@qq.com>
2023-04-25 23:35:27 +0800

    Mapper XML 的解析和注册使用

---
+++ mybatis-step03/src/main/java/com/yancy/mybatis/mapping/SqlCommandType.java
@@ -0,0 +1,33 @@
+package com.yancy.mybatis.mapping;
+
+/**
+ * @author yancy0109
+ */
+public enum SqlCommandType {
+
+    /**
+     * 未知
+     */
+    UNKNOWN,
+
+    /**
+     * 插入
+     */
+    INSERT,
+
+    /**
+     * 更新
+     */
+    UPDATE,
+
+    /**
+     * 删除
+     */
+    DELETE,
+
+    /**
+     * 查询
+     */
+    SELECT;
+
+}
