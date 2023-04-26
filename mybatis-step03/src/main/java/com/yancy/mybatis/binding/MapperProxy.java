9078501403bc286dacaee364c51a28c84d80f936
yancy0109 <1156729237@qq.com>
2023-04-26 21:11:49 +0800

    bugfix

--- mybatis-step03/src/main/java/com/yancy/mybatis/binding/MapperProxy.java
+++ mybatis-step03/src/main/java/com/yancy/mybatis/binding/MapperProxy.java
@@ -32,7 +32,7 @@ public class MapperProxy<T> implements InvocationHandler, Serializable {
         if (Object.class.equals(method.getDeclaringClass())) {
             return method.invoke(proxy, args);
         }
-        final MapperMethod mapperMethod = methodCache.get(method);
+        final MapperMethod mapperMethod = cachedMapperMethod(method);
         return mapperMethod.execute(sqlSession, args);
     }
 
